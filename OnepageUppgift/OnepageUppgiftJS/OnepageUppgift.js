b8702adbdb2825662ef0b985bd37b2bc54fff760
Laoudede <laoudede@gmail.com>
2019-03-27 22:32:10 +0100

    skript

--- OnepageUppgift/OnepageUppgiftJS/OnepageUppgift.js
+++ OnepageUppgift/OnepageUppgiftJS/OnepageUppgift.js
@@ -1,10 +1,22 @@
+/*$('#bildPåSida2').hidden();
+$('#textPåSida2').hidden();*/
+
 $("#button").click(function(){
 
 
   $('html, body').animate({
         scrollTop: $("header").offset().top
-    }, 4000);
+    }, 2000);
 
 
 
 });
+
+/*if($(window).scrollTop()>1080){
+
+
+  $('#bildPåSida2').fadeIn(5000, function());
+  $('#textPåSida2').fadeIn(5000, function());
+
+
+}*/
